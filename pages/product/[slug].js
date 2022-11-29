f63a316ec17956b46c1764c1f1c0fb2a5d19d83c
Ryan <ryanz35@hotmail.com>
2022-11-29 16:59:34 -0500

    getStaticPaths needed, bug on image load

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,58 @@
 import React from 'react'
+// images called 
+import { client, urlFor } from '../../lib/client';
 
-const ProductDetails = () => {
+const ProductDetails = (product, products) => {
+
+    // make it easier to call instead product.image
+    const { image, name, details, price } = product;
+
   return (
-    <div>Where the item will be displayed</div>
+    <div>
+        <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+                    <img src={urlFor(image && image[0])} />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+    const products = await client.fetch(query);
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback:'blocking'
+    }
+ }
+
+// from index.js but changing it a little for product page
+// using getStaticProps to pre-render page
+export const getStaticProps = async ({ params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]';
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+
+
+  
+    return {
+      props: { products, product }
+    }
+  }
 
 export default ProductDetails

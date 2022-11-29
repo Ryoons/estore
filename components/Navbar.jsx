0d6e923d99d99af0cfc5a6df2b6bbe0fa62f1485
Ryan <ryanz35@hotmail.com>
2022-11-29 15:40:07 -0500

    installing react icons, setting up nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">Juiced Desktops</Link>
+      </p>
+      <button 
+      type='button' 
+      className='cart-icon'
+      onClick="">
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 

07b9913c7e59e1a37376ad776e8ac087ae28e4b5
Ryan <ryanz35@hotmail.com>
2022-12-15 14:29:36 -0500

    cart imports

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlineLef, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import { Toast } from 'react-hot-toast';
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
+
 
 const Cart = () => {
   return (

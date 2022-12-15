89b7ed4976598d6d1339b1848b1155d2aaa31b3e
Ryan <ryanz35@hotmail.com>
2022-12-15 16:14:01 -0500

    item desc for cart, subtotal added

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import Link from 'next/link';
-import { AiOutlineMinus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { AiOutlineMinus, AiOutlineLeft, AiOutlineShopping, AiOutlinePlus } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
 import { Toast } from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
@@ -38,9 +38,49 @@ const Cart = () => {
         {cartItems.length >= 1 && cartItems.map((item) => (
           <div className='product' key={item._id}>
             <img src={urlFor(item?.image[0])} className='cart-product-image' />
+            <div className='item-desc'>
+              <div className='flex top'>
+                <h5>{item.name}</h5>
+                <h4>${item.price}</h4>
+              </div>
+              <div className='flex bottom'>
+                <div>
+                <p className='quantity-desc'>
+                        <span className='minus' onClick="">
+                            <AiOutlineMinus />
+                        </span>
+                        <span className='num' onClick="">
+                            0
+                        </span>
+                        <span className='plus' onClick="">
+                            <AiOutlinePlus />
+                        </span>
+                    </p>
+                </div>
+                <button
+                type='button'
+                className='remove-item'
+                onClick="">
+                  <TiDeleteOutline />
+                </button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
+      {cartItems.length >= 1 && (
+        <div cart-bottom>
+          <div className='total'>
+            <h3>Subtotal:</h3>
+            <h3>${totalPrice}</h3>
+          </div>
+          <div className='btn-container'>
+            <button type='button' className='btn' onClick="">
+              Pay Now
+            </button>
+          </div>
+        </div>
+      )}
       </div>
     </div>
   )

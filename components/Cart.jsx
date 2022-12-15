cbcdf06e7a1906edc1a4541bb2e9c0cca939b074
Ryan <ryanz35@hotmail.com>
2022-12-15 15:26:36 -0500

    shopping cart pdate

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import Link from 'next/link';
-import { AiOutlineMinus, AiOutlineLef, AiOutlineShopping } from 'react-icons/ai';
+import { AiOutlineMinus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
 import { Toast } from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
@@ -8,8 +8,19 @@ import { urlFor } from '../lib/client';
 
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, TotalQuantity, cartItems, setShowCart } = useStateContext();
+
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+      <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
+        <AiOutlineLeft />
+        <span className='heading'> Your Cart </span>
+        <span className='cart-num-items'>({TotalQuantity}Items)</span>
+      </button>
+      </div>
+    </div>
   )
 }
 

3e9a8995f4060b26df06f547465e62751b4479cc
Ryan <ryanz35@hotmail.com>
2022-12-20 17:00:55 -0500

    working on stripe api

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,1 +1,37 @@
-import Stripe from 'stripe';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+        const params = {
+            submit_type: 'pay',
+            mode: 'payment',
+            payment_method: ['card'],
+            billing_address_collection: 'auto',
+            shipping_options:[
+                { shipping_rate: 'shr_1MHDprKbJ6Y524PXMVlcNNDX' },
+                { shipping_rate: 'shr_1MHDqsKbJ6Y524PXSWGfl6ER'},
+            ],
+
+            line_items: [
+              {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price: '{{PRICE_ID}}',
+                quantity: 1,
+              },
+            ],
+            mode: 'payment',
+            success_url: `${req.headers.origin}/?success=true`,
+            cancel_url: `${req.headers.origin}/?canceled=true`,
+          }
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create();
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}

2a804e01ac0288983488f82f4dc41d8c7984b7a6
Ryan <ryanz35@hotmail.com>
2023-01-24 17:07:44 -0500

    button to return to home page

--- pages/success.js
+++ pages/success.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
-import { useRouter } from 'next/router';
 
 import { useStateContext } from '../context/StateContext';
 
 const Success = () => {
-
   const { setCartItems, setTotalPrice, setTotalQuantity } = useStateContext();
-
-  const [order, setOrder] = useState(null);
   
   return (
     <div className='success-wrapper'>
       <div className='success'>
         <p className='icon'>
           <BsBagCheckFill />
+        </p>
+        <h3>Thanks for odering from Juiced Desktops.</h3>
+        <p className='email-msg'>Confirmation has been sent to email.</p>
+        <p>
+          <Link href='/'>
+            <button type='button'>
+              Home
+            </button>
+          </Link>
         </p>
       </div>
     </div>

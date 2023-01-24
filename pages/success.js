3d70705f667c1ae9e0b236f5cbb6e37e50d810aa
Ryan <ryanz35@hotmail.com>
2023-01-24 17:01:43 -0500

    bag color change

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
-import { useRouter } from 'react-router';
+import { useRouter } from 'next/router';
 
 import { useStateContext } from '../context/StateContext';
 
